2cd89c5616114bb61a460df628050d6a5fddbbc9
mckgar <kmgarner23@outlook.com>
2022-07-07 22:21:49 -0600

    Added authentication

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const Member = require('./models/member');
 
 const app = express();
 
@@ -23,6 +24,43 @@ db.on('error', console.error.bind(console, 'mongo connection error'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+passport.use(
+  new LocalStrategy( async (username, password, done) => {
+    try {
+      const member = await Member.findOne({ username: username });
+      if (!member) {
+        return done(null, false, { message: 'Incorrect Credentials' });
+      }
+      const isValid =  await bcrypt.compare(password, member.password);
+      if (isValid) {
+        return done(null, member);
+      } else {
+        return done(null, false, { message: 'Incorrect credentials' });
+      }
+    } catch (err) {
+      done(err);
+    }
+  })
+);
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+  Member.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+app.use(session({ secret: process.env.SECRET_KEY, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
